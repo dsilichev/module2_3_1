bebbbb80852741449755e2d948f89ce22d99726e
Dmitry <dsilichev@gmail.com>
2024-03-21 13:19:49 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import styles from './app.module.css';
+
+export const App = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Крестики-нолики</h1>
+      
+    </div>
+  );
+};
