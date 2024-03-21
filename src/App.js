1f441eba5549660b9f176a8e73e834e9a9d56f2f
Dmitrii Ilichev <dsilichev@gmail.com>
2024-03-21 23:31:34 +0300

    states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState } from 'react';
 import styles from './app.module.css';
 import { GameLayout } from './components/GameLayout';
 
 export const App = () => {
+  let [currentPlayer, setCurrentPlayer] = useState('X');
+  let [isGameEnded, setIsGameEnded] = useState(false);
+  let [isDraw, setIsDraw] = useState(false);
+  let [field, setField] = useState([
+    '', '', '',
+    '', '', '',
+    '', '', '',
+  ]);
+
   return (
     <div className={styles.container}>
       <h1>Крестики-нолики</h1>
